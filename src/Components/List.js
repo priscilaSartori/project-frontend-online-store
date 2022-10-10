f57eea30ab7a6bf4a6bc2e09505bce2d5211257a
Stael Edson <edsonstael@gmail.com>
2022-10-10 16:00:56 -0300

    requisito 4

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.salvandoCategorias();
+  }
+
+  salvandoCategorias = async () => {
+    const categorias = await getCategories();
+    this.setState({
+      categories: categorias,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma
-        categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma
+          categoria.
+        </p>
+        {categories.map((categoria) => (
+          <button
+            type="button"
+            key={ categoria.id }
+            data-testid="category"
+          >
+            {categoria.name}
+          </button>
+        ))}
+      </div>
     );
   }
 }
